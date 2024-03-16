d9abbdec6c771cf78b00302a1a2d1b5a2b9bd553
wkwon <rnjsdntjr26@daum.net>
2024-03-16 17:33:19 +0900

    docs: ConcurrentKafkaListenerContainerFactory 설명 주석 추가

--- spring-kafka-consumer/src/main/java/org/example/springkafkaconsumer/config/MessageListenerContainerConfig.java
+++ spring-kafka-consumer/src/main/java/org/example/springkafkaconsumer/config/MessageListenerContainerConfig.java
@@ -18,23 +18,27 @@ import java.util.Map;
 
 @Configuration
 public class MessageListenerContainerConfig {
-//    @Bean // 토픽(의 파티션)으로부터 받는 모든 message를 single thread로 처리한다.
-//    public KafkaMessageListenerContainer<String, String> kafkaMessageListenerContainerFactory(
-//            ConsumerFactory<String, String> consumerFactory) {
-//        KafkaMessageListenerContainer<String, String> factory = new KafkaMessageListenerContainer<>(
-//                consumerFactory,
-//                // ContainerProperties 생성자는 3가지 종류가 있다. (String... topics / Pattern topicPattern / TopicPartitionOffset... topicPartitions)
-//                new ContainerProperties("test")
-//        );
-//
-//        // Message Listener을 할당해야 한다.
-//        // 참고롤 Message Listener의 종류는 8가지가 있다. (https://docs.spring.io/spring-kafka/reference/kafka/receiving-messages/message-listeners.html)
-//        factory.setupMessageListener((MessageListener<String, String>) data -> System.out.println(data));
-//
-//        return factory;
-//    }
+    @Bean // 토픽(의 파티션)으로부터 받는 모든 message를 single thread로 처리한다.
+    public KafkaMessageListenerContainer<String, String> kafkaMessageListenerContainerFactory(
+            ConsumerFactory<String, String> consumerFactory) {
+        KafkaMessageListenerContainer<String, String> factory = new KafkaMessageListenerContainer<>(
+                consumerFactory,
+                // ContainerProperties 생성자는 3가지 종류가 있다. (String... topics / Pattern topicPattern / TopicPartitionOffset... topicPartitions)
+                new ContainerProperties("test")
+        );
 
-    @Bean // multi-thread consume이 가능하도록 1개 이상의 KafkaMessageListenerContainer에게 message consume을 위임(delegate)한다.
+
+        // Message Listener을 할당해야 한다.
+        // 참고롤 Message Listener의 종류는 8가지가 있다. (https://docs.spring.io/spring-kafka/reference/kafka/receiving-messages/message-listeners.html)
+        factory.setupMessageListener((MessageListener<String, String>) data -> System.out.println(data));
+        return factory;
+    }
+
+    // multi-thread consume이 가능하도록 1개 이상의 KafkaMessageListenerContainer에게 message consume을 위임(delegate)한다.
+    // Thread 개수만큼의 partition을 동시에 처리할 수 있게 된다.
+    // 1개의 Thread는 한번에 1개의 partition으로부터 poll()을 할 수 있고 데이터를 처리한다.
+    // 때문에 여기서 설정하는 concurrency는 1개의 partition에서 가져온 message들을 동시 처리하는 것이 아님
+    @Bean
     public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaMessageListenerContainerFactory(
             ConsumerFactory<String, String> consumerFactory) {
         ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
@@ -43,8 +47,8 @@ public class MessageListenerContainerConfig {
         // Batch Listener를 사용하려면 필수적으로 true 설정을 해줘야 한다.
         factory.setBatchListener(true);
         factory.getContainerProperties()
-                .setPollTimeout(Duration.ofSeconds(5).toMillis());
-
+               .setPollTimeout(Duration.ofSeconds(5).toMillis());
+        // factory.setConcurrency(3); // 3개의 partition까지 동시 처리 가능. TaskExecutor를 설정하지 않았기 때문에 SimpleAsyncTaskExecutor가 등록됨
         return factory;
     }
 }
